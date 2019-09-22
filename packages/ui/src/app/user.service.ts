d8e21921ae53b67b54fd9f7f6b9ba4eef75bdd6f
Steve Black <stevenjblack@gmail.com>
2019-09-21 23:18:54 -0400

    Update user.service.ts
    * retrieves user `name` from localStorage on construction
    * synchronizes user `name` with localStorage via subscribe

--- packages/ui/src/app/user.service.ts
+++ packages/ui/src/app/user.service.ts
@@ -1,16 +1,33 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { storedObject } from './shared/utils';
+import { IUser } from './shared/models';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
   private nameSubject = new BehaviorSubject<string>('');
+  // private urlSubject = new BehaviorSubject<string>('');
+  // private avatarSubject = new BehaviorSubject<string>('');
+  // private emailSubject = new BehaviorSubject<string>('');
   name = this.nameSubject.asObservable();
+  // url = this.urlSubject.asObservable();
+  // avatar = this.avatarSubject.asObservable();
+  // email = this.emailSubject.asObservable();
 
   constructor() {
-    this.name.subscribe(() => {
+    const userStorageKey = 'loe:user';
+    const storedUser = storedObject.get(userStorageKey);
+    if (storedUser && storedUser.name) {
+      this.nameSubject.next(storedUser.name);
+    }
 
+    this.name.subscribe((name) => {
+      const user: IUser = {
+        name,
+      };
+      storedObject.set(userStorageKey, user);
     });
   }
 
