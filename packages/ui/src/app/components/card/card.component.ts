033f083c0b606a6220afe26e3322cd529065d7e1
Steve Black <stevenjblack@gmail.com>
2019-09-22 15:06:43 -0400

    `card` component updates

--- packages/ui/src/app/components/card/card.component.ts
+++ packages/ui/src/app/components/card/card.component.ts
@@ -3,12 +3,16 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+  styleUrls: ['./card.component.scss'],
 })
 export class CardComponent implements OnInit {
   @Input() estimate: number | string;
 
   constructor() { }
+
+  cardClick() {
+    alert(`You clicked, ${this.estimate}!`);
+  }
 
   ngOnInit() {
   }
